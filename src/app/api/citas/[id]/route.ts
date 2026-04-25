015b5f5b0f5ae5deb2abbd54fb7e191defde8fb8
Yoel Reyes <yoelreyes05@gmail.com>
2026-04-24 23:20:57 -0400

    Fix recuperar contraseña + sistema admin

--- src/app/api/citas/[id]/route.ts
+++ src/app/api/citas/[id]/route.ts
@@ -1,3 +1,6 @@
+
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 import jwt from "jsonwebtoken";
--- src/app/api/citas/route.ts
+++ src/app/api/citas/route.ts
@@ -1,3 +1,6 @@
+
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 import jwt from "jsonwebtoken";
